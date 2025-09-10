3147c29c230aacb0dd6eec36c3dff4a85f86f053
pedrini.edoardo <pedrini.edoardo@hsr.it>
2025-09-10 19:40:11 +0200

    add cacoa package

---
+++ analysis/R_analysis_R44/scr/00_libraries.R
@@ -0,0 +1,9 @@
+# installation of cacoa
+renv::install(c("clusterProfiler", "DESeq2", "DOSE", "EnhancedVolcano", "enrichplot", "fabia", "GOfuncR", "Rgraphviz"))
+renv::install("coda.base")
+renv::install("psych")
+renv::install("kernlab",type = "source")
+renv::install("quadprog",type = "source")
+renv::install("mclust",type = "source")
+renv::install("mvtnorm",type = "source")
+renv::install("kharchenkolab/cacoa")
