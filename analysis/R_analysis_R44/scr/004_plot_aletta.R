3b6298c5909dcb56ee685cf97a374709258aef52
pedriniedoardo <pedrini.edoardo@gmail.com>
2026-06-22 18:23:38 +0200

    add: script plot aletta

---
+++ analysis/R_analysis_R44/scr/004_plot_aletta.R
@@ -0,0 +1,43 @@
+# AIM ---------------------------------------------------------------------
+# queries from aletta
+
+# libraries ---------------------------------------------------------------
+library(Seurat)
+library(tidyverse)
+library(patchwork)
+
+# read in the data --------------------------------------------------------
+# read in the object
+sobj_update <- readRDS("../../out/object/analysis_R44/001_sobj_integrated_cleanup_manualAnnotation_updateDemyelination.rds")
+
+DimPlot(sobj_update,label = T)
+
+# analysis ----------------------------------------------------------------
+# aletta asked to provide a dotplot with a panel of maker genes per cell type
+Idents(sobj_update) <- "cell_id"
+
+shortlist_features_list_long <- list(
+  IMMUNE = c("LYVE1","CD163","MRC1","LINGO1","HSPA1A","MOBP","CD22","CD83","HIF1A","VEGFA","SOD1","TREM2","CX3CR1","P2RY12","C3","CSF1R", "CD74", "RUNX1","C1QB","PTPRC","AIF1","HLA-DRA","TYROBP"),
+  B_CELLS = c("IGHG1", "CD38"),
+  T_CELLS =  c("SKAP1", "CD8A", "CD2"),
+  OLIGOLINEAGE = c("PLP1","MOG","PPP1R16B","TNS3","HMGB1","CD81","B2M","C1QL1","HLA-A","HLA-C","NLGN4X","OLIG1","OLIG2","CSPG4", "PDGFRA", "SOX6", "SOX10", "MBP","MAG"),
+  ASTRO = c("AQP4", "GFAP", "CD44", "AQP1", "VIM","APOE", "VCAN", "STAT3", "ABCA1", "TNC", "SDC4","SLC1A2","S100B"),
+  NEURONS = c("GAD2", "PVALB", "SV2C", "VIP", "TLE4", "CUX2", "THY1", "SLC17A7", "NRGN", "SATB2", "RORB", "SST", "STX1A", "STX1B", "SYP", "TH", "NEFL","SYT1"),
+  ENDO = c("VWF","CDH5","TEK","PECAM1","FLT1","KDR","NOS3","MCAM","MMRN1","CLDN5","BMX","ANGPT2","GJA4","TIE1","ROBO4","ECSCR"),
+  PERICYTE = c("PDGFRB","DES","ACTA2","ANPEP","RGS5","ABCC9","KCNJ8","CD248","DLK1","NT5E","ANGPT1"),
+  SCHWANN = c("PMP22","MPZ","PRX"),
+  EPENDYMA = c("CFAP299","DNAH7","DNAH9"),
+  STROMAL = c("LAMA2","RBMS3","CEMIP","GPC6")
+)
+
+# plot the shortlisted feature per cluster
+# notice that this is done only on the subset of the young (control) cells
+test_long01 <- DotPlot(sobj_update,
+                       features = shortlist_features_list_long,
+                       dot.scale = 8,
+                       cluster.idents = T,
+                       group.by = "cell_id") +
+  RotatedAxis() +
+  labs(title = "cell_id")+
+  theme(strip.text = element_text(angle = 90))
+ggsave(plot=test_long01,"../../out/plot/analysis_R44/004_DotplotLong_cellid.pdf",width = 30,height = 6)
