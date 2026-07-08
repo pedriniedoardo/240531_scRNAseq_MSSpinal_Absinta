b0df9e5a38b4261dfd18dba7a862e7681d018172
pedriniedoardo <pedrini.edoardo@gmail.com>
2026-07-08 16:29:44 +0200

    fix: add EDA aletta subset LYM

--- analysis/R_analysis_R44/scr/27_subcluster_analysis_LYM_plot.R
+++ analysis/R_analysis_R44/scr/27_subcluster_analysis_LYM_plot.R
@@ -330,6 +330,26 @@ DimPlot(data.combined, reduction = "umap",group.by = "RNA_snn_res.0.4",split.by 
 ggsave("../../out/plot/analysis_R44/27_UMAPCluster_splitDisease_res0.4_LYM_subcluster.pdf",width = 8,height = 4)
 # DimPlot(data.combined, reduction = "umap", split.by = "origin",label = T,raster = T,ncol=5)
 
+# -------------------------------------------------------------------------
+# Aletta suggested using the following panel to identity the OLIGO
+shortlist_features_aletta <- list(
+  custom_aletta = c("IFNG", "TBX21", "EOMES", "NKG7", "PRF1", "GZMB", "CXCR3", "CCL5", "CXCL1", "CXCL2")
+  # ASTRO = c("AQP4","GFAP","CD44","AQP1","VIM","APOE","VCAN","STAT3","ABCA1","TNC","SDC4","SLC1A2","S100B"),
+  # NEURONS = c("GAD2","PVALB","SV2C","VIP","TLE4","CUX2","THY1","SLC17A7","NRGN","SATB2","RORB","SST","STX1A","STX1B","SYP","TH","NEFL","SYT1")
+)
+
+test_long_test <- DotPlot(data.combined,
+                          features = shortlist_features_aletta,
+                          dot.scale = 8,
+                          cluster.idents = T,
+                          group.by = "RNA_snn_res.0.4") +
+  RotatedAxis() +
+  labs(title = "RNA_snn_res.0.4")+
+  theme(strip.text = element_text(angle = 90))
+
+ggsave(plot=test_long_test,"../../out/plot/analysis_R44/27_DotplotAletta_res0.4_LYM_subcluster.pdf",width = 6,height = 6)
+# -------------------------------------------------------------------------
+
 # plot the shortlisted feature per cluster
 # notice that this is done only on the subset of the young (control) cells
 test_long01 <- DotPlot(data.combined,
